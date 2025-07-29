51d4e2c584bc14a89c575e0bf73b03cacd939385
Mkrish812 <mkris812.0@gmail.com>
2025-07-29 23:52:51 +0530

    This si the tailwind commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div className='flex justify-center items-center h-screen'>
-      This is my learning project 2
+      This is my learning project 2 git push -u origin main
     </div>
   )
 }
---
+++ src/layouts/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <>
+    <nav className='flex justify-between items-center bg-transparent backdrop-blur-3xl px-10 py-3 absolute w-screen'>
+        
+
+    </nav>
+    </>
+    
+  )
+}
