eb744e52ef32e0e5f6a3c70fa0aab2981b7e079a
Mkrish812 <mkris812.0@gmail.com>
2025-07-30 23:57:53 +0530

    started

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -8,13 +8,13 @@ export default function Home() {
         <div className="flex flex-col  items-center lg:items-start text-[clamp(1.25rem,4vw,3.75rem)] font-medium">
             <span className="">Grow Smarter,</span>
           
-          <span className="text-nowrap bg-gradient-to-b from-black via-black to-gray-100 bg-clip-text text-transparent">Invest with Confidence</span>
+          <span className="text-nowrap bg-gradient-to-b from-black via-gray-500 to-gray-300 bg-clip-text text-transparent">Invest with Confidence</span>
         </div>
         <div className="flex flex-col justify-between gap-5 items-center lg:items-end bg-">
           <p className="text-center lg:text-end text-gray-500">
             Smarter tools designed to help you grow your wealth effortlessly andtake control of your financial future.
           </p>
-          <button className="py-2 px-5 rounded-3xl text-white bg-blue-400 shadow-sm hover:shadow-lg hover:shadow-inner hover:shadow-black/20 shadow-black/20  ">
+          <button className="py-2 px-5 rounded-3xl text-white bg-blue-400 shadow-sm hover:shadow-lg  hover:shadow-black/20 shadow-black/20  ">
             Get Starded
           </button>
         </div>
--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   return (
     <>
       <nav className="flex justify-between items-center h-16 bg-transparen bg-gray-950t backdrop-blur-md px-10 lg:px-40 py-3 fixed top-0 w-screen">
-        <Link to="home" smooth={true} duration={500} className="text-sm font-light ">
+        <Link to="home" smooth={true} duration={500} className="text-sm font-blod ">
           The <span className="text-zinc-950">Company</span>
         </Link>
 
