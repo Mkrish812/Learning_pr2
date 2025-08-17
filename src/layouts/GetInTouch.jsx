6faabee491c3c345fbeceecb60fd101a4306c8a8
Mkrish812 <mkris812.0@gmail.com>
2025-08-18 00:15:06 +0530

    Completed

--- src/layouts/GetInTouch.jsx
+++ src/layouts/GetInTouch.jsx
@@ -44,7 +44,7 @@ export default function GetInTouch() {
 
   return (
     <Element name="getintouch" className="mt-10 py-10">
-      <div className="container">
+      <div className="container overflow-hidden">
         <div className="flex flex-col gap-5 py-10">
           <h3 className="mx-auto rounded-3xl bg-white px-5 py-1 text-sm text-gray-500 shadow-xl">
             Contact
